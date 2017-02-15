5e1681995bdc2331e5078ed163d2aa925a0b9068
Lloyxide <lloyxide@gmail.com>
2017-02-16 00:11:22 +0100

    1.0

---
+++ js/script.js
@@ -0,0 +1,40 @@
+function dogDogDog(){
+
+  var audioDog = new Audio('audio/dog.mp3');
+  audioDog.play();
+
+  $('#doggy').css("display", "none");
+
+  $('.dogInc').each(function (){
+    $(this).prop("src","images/dog.gif");
+    $(this).css({"width": "auto", "position": "fixed"});
+  });
+
+  setInterval(function(){
+    $('.dogInc').each(function (){
+
+      var randomtop = Math.floor(Math.random() * ($(window).height() - $(this).height() - 20));
+      var randomleft = Math.floor(Math.random() * ($(window).width() - $(this).width() - 20));
+
+      $(this).css('top', randomtop + 'px');
+      $(this).css('left', randomleft + 'px');
+
+      var rand = Math.floor((Math.random() * 3) + 1);
+      switch(rand){
+        case 1 : $(this).toggleClass("monster1"); break;
+        case 2 : $(this).toggleClass("monster2"); break;
+        case 3 : $(this).toggleClass("monster3"); break;
+      }
+    });
+  }, 500);
+
+}
+
+
+
+function menuMobile(){
+  if(document.getElementById("top_right_menu").style.display == "block")
+  document.getElementById("top_right_menu").style.display = "none";
+  else
+  document.getElementById("top_right_menu").style.display = "block";
+}
